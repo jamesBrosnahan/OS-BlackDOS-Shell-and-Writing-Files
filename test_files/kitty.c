a3a6b9a8a74fc3735566a751416e69ca73a37a74
James <jb172@zips.uakron.edu>
2017-01-20 11:15:24 -0500

    initial commit

---
+++ test_files/kitty.c
@@ -0,0 +1,14 @@
+#include "blackdos.h"
+
+void main()
+{
+   PRINTS("     |\\      _,,,---,,_\r\n\0");
+   PRINTS("     /,`.-\'`\'    -.  ;-;;,_\r\n\0");
+   PRINTS("    |,4-  ) )-,_..;\\ (  `\'-\'\r\n\0");
+   PRINTS("   \'---\'\'(_/--\'  `-\'\\_)\r\n\0");
+   PRINTS("Soft Kitty, Warm Kitty,\r\n\0");
+   PRINTS("  little ball of fur.\r\n\0");
+   PRINTS("    Happy Kitty, Sleepy Kitty,\r\n\0");
+   PRINTS("              purr, purr, purr.\r\n\0");
+   END;
+}
